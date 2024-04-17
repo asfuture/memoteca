2c8dfdc2a3d7d5ce9f6b69795309afac01bb7e41
Alex Ferreira <asfuture2019@gmail.com>
2024-04-17 10:55:14 -0300

    Primeiro commit
    Iniciando projeto memoteca.

---
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,34 @@
+import { Router } from '@angular/router';
+import { PensamentoService } from '../pensamento.service';
+import { Pensamento } from './../pensamento';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrls: ['./criar-pensamento.component.css']
+})
+export class CriarPensamentoComponent implements OnInit {
+[x: string]: any;
+pensamento: Pensamento =  {
+  conteudo:'',
+  autoria:'',
+  modelo:''
+}
+  constructor( 
+    private service: PensamentoService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  criarPensamento(){
+    this.service.criar(this.pensamento).subscribe(() => {
+      this.router.navigate(['/listarPensamento'])
+    })
+  }
+  cancelar() {
+    this.router.navigate(['/listarPensamento'])
+  }
+}
